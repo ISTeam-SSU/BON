47532267eaedd40e8273dda4792d858530546a08
강경완 <lk@lemonlab.co.kr>
2015-12-04 23:02:45 +0900

    1

--- app/src/main/java/kr/ac/ssu/bon/ui/RecyclerViewFragment.java
+++ app/src/main/java/kr/ac/ssu/bon/ui/RecyclerViewFragment.java
@@ -2,6 +2,8 @@ package kr.ac.ssu.bon.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -72,5 +74,14 @@ public class RecyclerViewFragment extends Fragment {
         }
 
         MaterialViewPagerHelper.registerRecyclerView(getActivity(), mRecyclerView, null);
+
+//        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
